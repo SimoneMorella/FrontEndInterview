8a9bc2911cb4e1ee631ab00b21c9304354532bba
SimoneMorella <simonemorella21@gmail.com>
2024-09-06 16:52:20 +0200

    added real code fix

--- src/component/CodeTwoA.tsx
+++ src/component/CodeTwoA.tsx
@@ -1,15 +1,15 @@
 
 
 const codeString = `
-function getMaxFrequente(arr: number[]): number {
+function getMaxFrequente(arr) {
     // self explanatory code: just created an object where keys are numbers and values are their frequencies
-    const countObj = arr.reduce((obj: Record<number, number>, num) => {
+    let countObj = arr.reduce((obj, num) => {
         obj[num] = (obj[num] || 0) + 1;
         return obj;
     }, {});
     // found the most frequent number by sorting the keys in descending order and picking the first one (highest frequency) 
-    const mostFrequent = Object.keys(countObj).sort((a, b) => countObj[+b] - countObj[+a])[0];
-    return Number(mostFrequent);
+    let mostFrequent = Object.keys(countObj).sort((a, b) => countObj[b] - countObj[a])[0];
+    return mostFrequent;
 }
 `;
 
